0d164069be932585668356441ef1b9109a41e857
Julien CORON <julien.coron@gmail.com>
2021-11-26 17:48:35 +0100

    Update control input

--- script.py
+++ script.py
@@ -4,6 +4,7 @@ import psp2d
 import pspos
 import stackless
 from time import time
+import math
 
 ## screen size: 480 × 272 pixels
 MAX_WIDTH = 480
@@ -88,18 +89,8 @@ class Render():
             agent.update(self.walls)
 
     def draw(self):
-        # Each frame the renderer clears the screen,
+        # Each frame the renderer apply the background,
         # writes the text and draws each registered agent.
-        #background_color_1 = psp2d.Color(42,70,29,255)
-        #background_color_2 = psp2d.Color(42,60,29,255)
-        #self.screen.clear(background_color_1)
-
-        ## 480 = 15 * 32
-        ## 272 = 8.5 * 32
-        #for i in range(0, 15):
-        #    for j in range(0, 9):
-        #        if (i%2 and (j+1)%2) or ((i+1)%2 and j%2):
-        #            self.screen.fillRect(i*32,j*32,32,32, background_color_2)
         self.screen.blit(self.background, 0, 0, 480, 272, 0, 0, True)
         player = self.agents[0]
         font.drawText(self.screen, 0, 0, "(%d,%d) - Press O to exit" % (player.pos_x, player.pos_y))
@@ -131,25 +122,51 @@ class Player(Agent):
 
     def compute_new_position(self):
         pad = psp2d.Controller()
-        if pad.cross:
-            print("pad.analogX: %d // pad.analogY: %d" % (pad.analogX, pad.analogY))
-        dx = (pad.analogX / 127) * self.velocity
-        dy = (pad.analogY / 127) * self.velocity
+        (dx, dy) = (0, 0)
+        if pad.down:
+            dy = 1 * self.velocity
+            self.direction = "DOWN"
+        elif pad.up:
+            dy = -1 * self.velocity
+            self.direction = "UP"
+        elif pad.left:
+            dx = -1 * self.velocity
+            self.direction = "LEFT"
+        elif pad.right:
+            dx = 1 * self.velocity
+            self.direction = "RIGHT"
+        else:
+            dx = (pad.analogX / 127) * self.velocity
+            dy = (pad.analogY / 127) * self.velocity
+            if dx == 0 and dy == 0:
+                self.animation_flow = 0
+            else:
+                angle = int(math.degrees(math.atan2(dy, dx)) + 180)
+                # 360 = left
+                # 270 = down
+                # 180 = right
+                # 90 = up
+                if angle > 45 and angle <= 135:
+                    self.direction = "UP"
+                elif angle > 135 and angle <= 225:
+                    self.direction = "RIGHT"
+                elif angle > 225 and angle <= 315:
+                    self.direction = "DOWN"
+                else:
+                    self.direction = "LEFT"
+
+        if dx == 0 and dy == 0:
+            self.is_running = False
+        else:
+            if not self.is_running:
+                self.is_running = True
+                # Force to restart the animation to frame #0
+                self.animation_flow = 0
+
         new_pos_x = min(self.pos_x + dx, MAX_WIDTH - self.width)
         new_pos_x = max(new_pos_x, 0)
         new_pos_y = min(self.pos_y + dy, MAX_HEIGHT - self.height)
         new_pos_y = max(new_pos_y, 0)
-
-        #if dx>0 and dy>0:
-        #    if dx > dy:
-        #        self.direction = "LEFT"
-        #    else:
-        #        self.direction = "UP"
-        #elsif:
-        #    if dx > dy:
-        #        self.direction = "LEFT"
-        #    else:
-        #        self.direction = "UP"
 
         return (new_pos_x, new_pos_y)
 
@@ -164,11 +181,6 @@ class Player(Agent):
         if not self.detect_collision(walls, 
                                      new_pos_x + (self.shadow_with/2), 
                                      new_pos_y + (self.shadow_height/2)):
-            if not self.is_running and self.pos_x != new_pos_x and self.pos_y != new_pos_y :
-                self.is_running = True
-                # Force to restart the animation to frame #0
-                self.animation_flow = 0
-
             self.pos_x = new_pos_x
             self.pos_y = new_pos_y
             
@@ -179,15 +191,12 @@ class Player(Agent):
         left = image_bank[1]
         screen.blit(self.sprite, top, left, self.width, self.height, self.pos_x, self.pos_y, True)
         if self.is_running:
+            # One more step of the animation
+            self.animation_flow = self.animation_flow + 0.25
             if self.animation_flow > len(self.split[self.direction])-1:
                 # Restart the animation from the beginning
                 self.animation_flow = 0
-                self.is_running = False
-            else:
-                # One more step of the animation
-                self.animation_flow = self.animation_flow + 0.25
-
-# Creates the screen and its background color (Black)
+                
 
 # Loads the font
 font = psp2d.Font('font.png')
